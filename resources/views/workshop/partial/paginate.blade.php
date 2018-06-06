5946bcb3220cd1e10cbfe7fa4141fa707d50ba71
Tran Thanh <lethanhtdt@gmail.com>
2018-06-06 21:58:06 +0700

    update code pagination

--- resources/views/workshop/partial/paginate.blade.php
+++
@@ -1,7 +0,0 @@
-<nav class="pagination">
-    <span class="pagination__page pagination__page--current">1</span>
-    <a href="#" class="pagination__page">2</a>
-    <a href="#" class="pagination__page">3</a>
-    <a href="#" class="pagination__page">4</a>
-    <a href="#" class="pagination__page pagination__icon pagination__page--next"><i class="ui-arrow-right"></i></a>
-</nav>
